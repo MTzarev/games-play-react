0b82c5f86b417b04eba8ffe3a2a707f61e4c325a
Miroslav Tzarev <mtzarev@gmail.com>
2025-01-02 23:42:47 +0200

    Update gameService.js

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -4,6 +4,7 @@ export function getAll() {
         .then(res => res.json())
         
 }
+
 export let getOne = (id) => fetch (`${baseUrl}/games/${id}`).then(res=>res.json());
 export let getLatest = () => {
     return fetch (`${baseUrl}/games?sortBy=_createdOn%20desc&distinct=category`)
