9113cc4c02c583763166312f89679f999aee3ba4
Miroslav Tzarev <mtzarev@gmail.com>
2025-01-02 23:41:43 +0200

    Update ErrorPage.js

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -2,7 +2,6 @@ function ErrorPage () {
     return (
         <section id="catalog-page">
         <h1>Don't do that!</h1>
-        
                 <img style={{paddingLeft: 150 }} src="/images/danger.png" alt=""/>
     </section>
     );
